883fc879798c7e52c235051635e5f7e46807c2e9
ali kesgin <akakesgin@gmail.com>
2018-07-23 14:36:35 +0300

    new version

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@ router.post('/test', (req,res,next)=> {
 });
 router.post('/getByFood', (req,res,next)=> {
 
+    console.log(req.body);
     desiredFood = req.body.food;
     Siparis.getSiparisByFood(desiredFood, (err, foundSiparis) => {
         if(err) throw err;
@@ -21,12 +22,30 @@ router.post('/getByFood', (req,res,next)=> {
     });
 
 });
-router.post('/yenisiparis', (req,res,next)=> {
+router.post('/getToday', (req,res,next)=> {
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth(); 	
+    Siparis.getSiparisByToday(day, month, (err, foundSiparis) => {
+        if(err) throw err;
+        if(!foundSiparis){
+            return res.json({success: false, msg: "siparis may not found"})
+        }else{
+            res.json({siparis:foundSiparis});
+        }
+    });
 
+});
+router.post('/yenisiparis', (req,res,next)=> {
+    var d = new Date();
+    var day = d.getDate();
+    var month = d.getMonth();
     let newSiparis = new Siparis({
         username : req.body.username,
         food : req.body.food,
-        drink: req.body.drink
+        drink: req.body.drink,
+        day: day,
+        month: month
     });
     console.log(req.body);
     Siparis.addSiparis(newSiparis,(err, siparis) => {
