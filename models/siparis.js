2d06976dfc7f3a64da3fc654df88a1a24fbd9dba
ali kesgin <akakesgin@gmail.com>
2018-07-23 14:37:18 +0300

    new version

--- models/siparis.js
+++ models/siparis.js
@@ -5,7 +5,9 @@ const SiparisSchema = mongoose.Schema({
 
     username:{  type:String},
         food: {type:String},
-        drink: {type: String}
+        drink: {type: String},
+        day: {type: Number},
+        month: {type : Number}
 });
 
 const Siparis = module.exports = mongoose.model('Siparis', SiparisSchema);
@@ -16,5 +18,10 @@ module.exports.addSiparis = function(newSiparis, callback){
 
 module.exports.getSiparisByFood = function(desiredFood , callback){
     const query = {food: desiredFood};
+    Siparis.find(query,callback);
+}
+
+module.exports.getSiparisByToday = function(day, month , callback){
+    const query = {day: day , month: month};
     Siparis.find(query,callback);
 }
